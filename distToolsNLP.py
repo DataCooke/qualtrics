529f86851d077685f74a08ad3245ce0e7db30c45
Jon Cooke <cooke.jon.mba@gmail.com>
2020-12-07 13:28:08 -0700

    adding newer stuffs

--- distToolsNLP.py
+++ distToolsNLP.py
@@ -16,9 +16,48 @@ from nltk import word_tokenize
 from google.cloud import bigquery
 import pickle
 
+pip install --upgrade google-cloud-bigquery
 
-clientID = os.getenv('clientID')
-clientsecret = os.getenv('clientsecret')
+#### accessing required keys
+
+def access_secret_version(project_id, secret_id, version_id):
+    global payload
+    """
+    Access the payload for the given secret version if one exists. The version
+    can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
+    """
+
+    # Import the Secret Manager client library.
+    from google.cloud import secretmanager
+
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+
+    # Access the secret version.
+    response = client.access_secret_version(request={"name": name})
+
+    # Print the secret payload.
+    #
+    # WARNING: Do not print the secret in a production environment - this
+    # snippet is showing how to access the secret material.
+    payload = response.payload.data.decode("UTF-8")
+    access_secret_version.variable = payload
+    return(payload)
+
+### qualtrics key
+
+access_secret_version("nu-skin-corp", "dist-tools-nlp-qualtrics-client-secret", "latest")
+clientsecret = payload
+
+### qualtrics client ID
+
+access_secret_version("nu-skin-corp", "dist-tools-nlp-qualtrics-client-id", "latest")
+clientID = payload
+
 
 #create the Base64 encoded basic authorization string
 clientID=f"{clientID}"
