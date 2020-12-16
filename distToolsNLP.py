4a94d6a9b97298948055d1a53daf32e3055e22ff
Jon Cooke <cooke.jon.mba@gmail.com>
2020-12-16 16:09:58 -0700

    cleaned up and commented code

--- distToolsNLP.py
+++ distToolsNLP.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3.7
+### edit startDate and endDate to specify the data desired from qualtrics
+### wrap entire code as function called "all".
+### When running code locally, comment out the "all" function, comment out "pub_check" function at bottom of script
+### AND get rid of indent for ALL of the code that was wrapped in "all"
+### You will also need to edit the filepath where the parquet file is dumped (around line 332).
+### The "/tmp" portion should be removed when run locally from the to_parquet and "open" function.
+### nltk.downloads are only needed when runnin in cloud or on first run when run locally
 
 import http.client
 import http.client
 import zipfile
-import os
 import io
 import base64
 import json
@@ -18,6 +24,8 @@ import pickle
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
+### Build startDate and endDate variables beginning on the first day of the previous month and ending on the last day of the previous month
+
 today = date.today()
 d = today - relativedelta(months=1)
 
@@ -30,14 +38,19 @@ last_day = date(today.year, today.month, 1) - relativedelta(days=1)
 print("endDate: " + last_day.strftime("%Y-%m-%d"))
 endDate = last_day.strftime("%Y-%m-%d")
 
+### End building startDate and endDate
+
+### download resources needed for use in cloud function. This is only needed when running on console on first run locally
 
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('punkt')
 
+
+
 def all():
 
-    #### accessing required keys
+    #### defining function for accessing required keys from google secret manager
 
     def access_secret_version(project_id, secret_id, version_id):
         global payload
@@ -59,24 +72,24 @@ def all():
         # Access the secret version.
         response = client.access_secret_version(request={"name": name})
 
-        # Print the secret payload.
-        #
+
         # WARNING: Do not print the secret in a production environment - this
         # snippet is showing how to access the secret material.
         payload = response.payload.data.decode("UTF-8")
         access_secret_version.variable = payload
         return(payload)
 
-    ### qualtrics key
+    ### calling function to get qualtrics key from secret manager
 
     access_secret_version("nu-skin-corp", "dist-tools-nlp-qualtrics-client-secret", "latest")
     clientsecret = payload
 
-    ### qualtrics client ID
+    ### calling function to get qualtrics client id from secret manager
 
     access_secret_version("nu-skin-corp", "dist-tools-nlp-qualtrics-client-id", "latest")
     clientID = payload
 
+    ### use qualtrics client id and client secret to request qualtrics key required to pull data
 
     #create the Base64 encoded basic authorization string
     clientID=f"{clientID}"
@@ -98,46 +111,32 @@ def all():
     res = conn.getresponse()
     data = res.read()
     token = json.loads(data.decode("utf-8"))
-    #print(token)
     token = token["access_token"]
-    #os.environ["token"] = token
 
+    ### final qualtrics key has been pulled. Moving on to pulling data.
+
+    ### Below only needed if you want to see what user and user id is used for qualtrics data
 
     # create the request
-    conn = http.client.HTTPSConnection("co1.qualtrics.com")
-    body = ''
-    headers = {
-    'Authorization': f'Bearer {token}'
-    }
+    #conn = http.client.HTTPSConnection("co1.qualtrics.com")
+    #body = ''
+    #headers = {
+    #'Authorization': f'Bearer {token}'
+    #}
 
     # make the request
-    conn.request("GET", "/API/v3/whoami", body, headers)
-    res = conn.getresponse()
-    data = res.read()
+    #conn.request("GET", "/API/v3/whoami", body, headers)
+    #res = conn.getresponse()
+    #data = res.read()
     #print(data.decode("utf-8"))
 
+    ### Above only needed if you want to see what user and user id is used for qualtrics data
 
-
-
-    # Setting user Parameters
-    dataCenter = "co1"
-
-    bearerToken = token # call the function defined in the previous code example
-    baseUrl = "https://{0}.qualtrics.com/API/v3/surveys".format(dataCenter)
-    headers = {
-        "authorization": "bearer " + bearerToken,
-        }
-
-    response = requests.request("GET", baseUrl, headers=headers)
-    #print(response.text)
-
-    #end of test api call
-    #added statics stuff
-
+    # Initialize requestCheckProgress and progressStatus
     requestCheckProgress = 0.0
     progressStatus = "inProgress"
 
-    #end added stuff
+    # creating parameters for qualtrics data pull.
 
     bearerToken = token # call the function defined in the previous code example
     baseUrl = "https://co1.qualtrics.com/API/v3/surveys/SV_8wbVPpmD5l5ItCt/export-responses/"
@@ -156,9 +155,6 @@ def all():
     downloadRequestResponse = requests.request("POST", baseUrl, headers=headers, json=body)
     progressId = downloadRequestResponse.json()["result"]["progressId"]
     print(downloadRequestResponse.text)
-
-
-
 
     # Step 2: Checking on Data Export Progress and waiting until export is ready
     while progressStatus != "complete" and progressStatus != "failed":
@@ -190,11 +186,15 @@ def all():
             dataDf = pd.read_csv(request.open(request.namelist()[0]), usecols=use_cols)
             print('*** DATA PULL COMPLETE ***')
 
+    ### data pulled from qualtrics complete
+
     print(" ")
     ###prepare file for future output
     ''' output will include duplicate values for ALL columns where a user answered multiple questions. 
     Each row represents one response where a single user could answer multiple questions and thus multiple responses.
     Therefore, when counting the number of Chrome browsers, you need to sum unique responses where browser is chrome'''
+
+    ### fix the headers/column heads
 
     dataDf.columns = dataDf.iloc[1]
     dataDf = dataDf.drop([0,1])
@@ -212,6 +212,8 @@ def all():
         , '{\"ImportId\":\"QID83_TEXT_TRANSLATEDeneh72e33\"}':'q83'
     })
 
+    ### create list of responses for each question
+
     DfQ29 = dataDf.drop(columns=['q3', 'q83'])
     DfQ3 = dataDf.drop(columns=['q29', 'q83'])
     DfQ83 = dataDf.drop(columns=['q29', 'q3'])
@@ -224,18 +226,20 @@ def all():
     DfQ3 = DfQ3.dropna(axis=0, subset=['response'])
     DfQ83 = DfQ83.dropna(axis=0, subset=['response'])
 
+    ### create new column indicating which question each response belongs to
+
     DfQ29["question"] = "q29"
     DfQ3["question"] = "q3"
     DfQ83["question"] = "q83"
 
-    #DfQ29.to_csv("DfQ29.csv")
-    #DfQ3.to_csv("DfQ3.csv")
-    #DfQ83.to_csv("DfQ83.csv")
+    ### stack all resonses on top of one another
 
     all_dfs = [DfQ29, DfQ3, DfQ83]
     for df in all_dfs:
         df.columns = ["startDate", "responseID", "userLanguage", "browser", "os", "currUrl", "response", "question"]
     all_dfs = pd.concat(all_dfs).reset_index(drop=True)
+
+    ### process response data for classification
 
     classifyData = all_dfs['response']
     classifyData = pd.DataFrame(classifyData)
@@ -246,6 +250,9 @@ def all():
     classifyData['response'] = classifyData['response'].apply(lambda x: ' '.join([item for item in x.split() if item not in stop_words]))
 
     w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
+
+    ### lemmatize - turning words into their root words
+
     lemmatizer = WordNetLemmatizer()
 
     def lemmatize_text(text):
@@ -302,6 +309,8 @@ def all():
 
     results = (nlpModel.classify_many(classifyFeatures))
 
+    ### initialize table with classification results from previously organized all_dfs
+
     all_dfsResults = all_dfs
 
     ### add classification as column "sentiment" to dataframe for upload to bigquery
@@ -318,16 +327,17 @@ def all():
 
 
     ### Convert data to parquet file
+    ### parquet file is used because big query can ingest the data much more easily by autodetecting data types and such
 
-    all_dfsResults.to_parquet("/tmp/parquetDat.parquet", compression='gzip', engine='fastparquet', index=False)
+    all_dfsResults.to_parquet("parquetDat.parquet", compression='gzip', engine='fastparquet', index=False)
 
-    # notice the lack of header skipping and schema detection parameters
+    # notice the lack of header skipping and schema detection parameters - due to parquet file magic
     client = bigquery.Client()
     table_id = 'nu-skin-corp.REPORTING.TEST_DIST_TOOLS_NLP'
     job_config = bigquery.LoadJobConfig()
     job_config.source_format = bigquery.SourceFormat.PARQUET
 
-    with open("/tmp/parquetDat.parquet", "rb") as source_file:
+    with open("parquetDat.parquet", "rb") as source_file:
         job = client.load_table_from_file(
             source_file,
             table_id,
@@ -336,6 +346,9 @@ def all():
 
     print(all_dfsResults)
     print("Upload Complete")
+
+### data uploaded to bigquery
+
 
 def pub_check(data, context):
     if 'data' in data:
